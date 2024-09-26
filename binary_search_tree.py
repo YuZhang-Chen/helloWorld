cfe4bf1a9fd631f3b2b4881cc8bf348f5f429fc6
YuZhang-Chen <C112156205@nkust.edu.tw>
2024-09-26 11:34:33 +0800

    first tree

---
+++ binary_search_tree.py
@@ -0,0 +1,67 @@
+class TreeNode:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, key):
+        if self.root is None:
+            self.root = TreeNode(key)
+        else:
+            self._insert(self.root, key)
+
+    def _insert(self, node, key):
+        if key < node.val:
+            if node.left is None:
+                node.left = TreeNode(key)
+            else:
+                self._insert(node.left, key)
+        else:
+            if node.right is None:
+                node.right = TreeNode(key)
+            else:
+                self._insert(node.right, key)
+
+    def search(self, key):
+        return self._search(self.root, key)
+
+    def _search(self, node, key):
+        if node is None or node.val == key:
+            return node
+        if key < node.val:
+            return self._search(node.left, key)
+        return self._search(node.right, key)
+
+    def inorder(self):
+        return self._inorder(self.root)
+
+    def _inorder(self, node):
+        res = []
+        if node:
+            res = self._inorder(node.left)
+            res.append(node.val)
+            res = res + self._inorder(node.right)
+        return res
+
+# 使用範例
+bst = BinarySearchTree()
+bst.insert(50)
+bst.insert(30)
+bst.insert(20)
+bst.insert(40)
+bst.insert(70)
+bst.insert(60)
+bst.insert(80)
+
+print("Inorder traversal of the given tree")
+print(bst.inorder())
+
+print("\nSearch for 40 in the tree")
+print(bst.search(40) is not None)
+
+print("\nSearch for 100 in the tree")
+print(bst.search(100) is not None)
